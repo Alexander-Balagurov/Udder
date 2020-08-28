8373535224532a82cfd5851ae0592a9503e4730c
Alexander Balagurov <entrue19@gmail.com>
2020-08-28 14:05:09 +0300

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Александр Балагуров on 28.08.2020.
+//  Copyright © 2020 Александр Балагуров. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
